085677facae13e77a9bcfc2d0f79df8d6925f84f
Kenzo Takahashi <kenzotakahashi2@gmail.com>
2015-02-20 16:55:54 -0800

    first commit

---
+++ client/client.js
@@ -0,0 +1,188 @@
+Meteor.subscribe("userData");
+Meteor.subscribe("matches");
+Meteor.subscribe("chat");
+
+Template.login.events({
+  'submit form': function(e, template){
+    e.preventDefault();
+    var user = {
+      email: $(e.target).find('[name=email]').val(),
+      password: $(e.target).find('[name=password]').val()
+    };
+    Meteor.loginWithPassword(user.email, user.password, function(error){
+      if (error){
+        console.log(error.reason);
+      } else {
+        if (Meteor.user().profile.type === 'company') {
+          console.log('type is company');
+          Router.go('/candidates');
+        } else {
+          Router.go('/jobs');
+        }
+      }
+    });
+  },
+});
+
+
+Template.jobSignup.events({
+  'submit form': function(e, template) {
+    e.preventDefault();
+    var user = {
+      name: $(e.target).find('[name=name]').val(),
+      email: $(e.target).find('[name=email]').val(),
+      password: $(e.target).find('[name=password]').val()
+    };
+
+    Accounts.createUser({
+      email: user.email,
+      password: user.password,
+      profile: {
+        name: user.name,
+        type: 'jobSeeker',
+        location: '',
+        summary: '',
+        interested: [],
+        match: [],
+      }
+    });
+
+    Router.go('jobs');
+  }
+});
+
+Template.companySignup.events({
+  'submit form': function(e, template) {
+    e.preventDefault();
+    var user = {
+      name: $(e.target).find('[name=name]').val(),
+      company: $(e.target).find('[name=copmany]').val(),
+      email: $(e.target).find('[name=email]').val(),
+      password: $(e.target).find('[name=password]').val()
+    };
+
+    Accounts.createUser({
+      email: user.email,
+      password: user.password,
+      profile: {
+        name: user.name,
+        copmany: user.company,
+        type: 'company',
+        location: '',
+        summary: '',
+        interested: [],
+        match: [],
+      }
+    });
+
+    Router.go('jobListing');
+  }
+});
+
+
+Template.jobListing.events({
+  'submit form': function(e, template) {
+    e.preventDefault();
+    profile = {
+      'profile.position': $(e.target).find('[name=position]').val(),
+      'profile.location': $(e.target).find('[name=location]').val(),
+      'profile.skillset': $(e.target).find('[name=skillset]').val().split(","),
+      'profile.description': $(e.target).find('[name=description]').val(),
+      'profile.url': $(e.target).find('[name=url]').val(),
+    };
+    Meteor.call('edit', profile, function(error, result) {
+      if (error)
+        return throwError(error.reason);
+      Router.go('candidates');
+    });
+  }
+});
+
+Template.matches.helpers({
+  'matches': function(){
+    if (Meteor.user().profile.type === 'company') {
+      return Matches.find({'company': Meteor.userId()});
+    } else {
+      return Matches.find({'jobSeeker': Meteor.userId()});
+    }
+  },
+  'getUser': function(){
+    // 'this' is a match object
+    if (Meteor.user().profile.type === 'company') {
+      return Meteor.users.findOne(this.jobSeeker).profile.name;
+    } else {
+      return Meteor.users.findOne(this.company).profile.company;
+    }
+  }
+});
+
+Template.chat.helpers({
+  'messages': function(){
+    return Chat.find({match: this.matchId});
+  },
+  'senderName': function(sender){
+    return Meteor.users.findOne(sender).profile.name;
+  },
+  'myLine': function(sender){
+    return !! (sender === Meteor.userId());
+  }
+});
+
+Template.chat.events({
+  'click .send, submit form': function(e, template){
+    e.preventDefault();
+
+    var $body = $('#message');
+    var message = {
+      match: this.matchId,
+      line: $body.val()
+    };
+
+    Meteor.call('sendMessage', message, function(error, result){
+      if (error) {
+        return throwError(error.reason);
+      } else {
+        $body.val('');
+      }
+    });
+  }
+});
+
+Template.job.events({
+  'click .interested': function(e, template) {
+    Meteor.call('interested', template.data._id, function(error, result) {
+      if (error)
+        return throwError(error.reason);
+      console.log('success');
+    });
+  }
+});
+
+Template.candidate.events({
+  'click .interested': function(e, template) {
+    Meteor.call('interested', template.data._id, function(error, result) {
+      if (error)
+        return throwError(error.reason);
+      console.log('success');
+    });
+  }
+});
+
+Template.nav.helpers({
+  'name': function() {
+    return Meteor.user().profile.name;
+  }
+});
+
+
+Template.nav.events({
+  'click #logout': function(e, template) {
+    e.preventDefault();
+    Meteor.logout();
+    Router.go('/');
+  },
+  'click .matches': function(e, template) {
+    e.preventDefault();
+    Router.go('/chat/' + Meteor.user().profile.match[0]);
+  }
+});
---
+++ server/server.js
@@ -0,0 +1,74 @@
+Meteor.publish("matches", function(){
+  return Matches.find();
+});
+
+Meteor.publish("chat", function(){
+  return Chat.find();
+});
+
+Meteor.publish("userData", function (){
+  // if (this.userId) {
+  //   return Meteor.users.find({_id: this.userId},
+  //                            {fields: {'other': 1, 'things': 1}});
+  // } else {
+  //   this.ready();
+  // }
+  return Meteor.users.find();
+});
+
+
+// Accounts.onCreateUser(function(options, user) {
+//   // We still want the default hook's 'profile' behavior.
+//   if (options.profile)
+//     profile = {
+//       type: null,
+//       location: '',
+//       description: '',
+//       meet: [],
+//       pass: [],
+//       match: [],
+//     }
+//
+//   return user;
+// });
+//
+
+
+Meteor.startup(function () {
+  if (Meteor.users.find().count() === 0){
+    for (var i = 0; i < 10; i++){
+      Accounts.createUser({
+        email: 'company' + i + '@gmail.com',
+        password: 'pass' + i,
+        profile: {
+          type: 'company',
+          name: 'CEO ' + i,
+          company: 'company ' + i,
+          location: 'San Francisco',
+          url: 'http://dropbox.com',
+          position: 'Full Stack Developer',
+          skillset: 'Meteor,Javascipt,CSS,HTML',
+          description: 'Dropbox is a free service that lets you bring your photos, docs, and videos anywhere and share them easily. Dropbox was founded in 2007 by Drew Houston and Arash Ferdowsi, two MIT students tired of emailing files to themselves to work from more than one computer.',
+          logo: '',
+          interested: [],
+          match: []
+        }
+      });
+    }
+
+    for (var i = 0; i < 10; i++){
+      Accounts.createUser({
+        email: 'candidate' + i + '@gmail.com',
+        password: 'pass' + i,
+        profile: {
+          type: 'jobSeeker',
+          name: 'candidate ' + i,
+          location: 'San Francisco',
+          summary: "I'm an Internet guy who believes in the triumph of humanity with a little help from technology. My current job is CEO of Jelly",
+          interested: [],
+          match: []
+        }
+      });
+    }
+  }
+});
